d08b79533032985ea13aaa301fef635bfb2ac675
nishanth <nishanthpkashyap1503@gmail.com>
2023-06-25 22:04:14 +0530

    removed .class files

--- src/main/java/com/nishanth/Hibernate_Demo/_1_App.java
+++ src/main/java/com/nishanth/Hibernate_Demo/_1_App.java
@@ -12,9 +12,9 @@ public class _1_App
     public static void main( String[] args )
     {
         _1_Alien alien1 = new _1_Alien();
-        alien1.setId(4);
-        alien1.setName("Ton 618");
-        alien1.setColour("Black");
+        alien1.setId(3);
+        alien1.setName("Hakaishin");
+        alien1.setColour("Purple");
         
         Configuration con = new Configuration().configure().addAnnotatedClass(_1_Alien.class);
         ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
