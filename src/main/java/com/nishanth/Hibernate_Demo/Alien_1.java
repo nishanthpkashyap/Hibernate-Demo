6a5e0ae6904790b39d3f9e5fbfe675a8b4602eb1
nishanth <nishanthpkashyap1503@gmail.com>
2023-06-25 22:20:07 +0530

    changed class names

--- src/main/java/com/nishanth/Hibernate_Demo/Alien_1.java
+++ src/main/java/com/nishanth/Hibernate_Demo/Alien_1.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class _1_Alien 
+public class Alien_1 
 {
 	@Id
 	private int id;
--- src/main/java/com/nishanth/Hibernate_Demo/_1_App.java
+++ src/main/java/com/nishanth/Hibernate_Demo/_1_App.java
@@ -11,12 +11,12 @@ public class _1_App
 {
     public static void main( String[] args )
     {
-        _1_Alien alien1 = new _1_Alien();
-        alien1.setId(3);
-        alien1.setName("Hakaishin");
-        alien1.setColour("Purple");
+        Alien_1 alien1 = new Alien_1();
+        alien1.setId(6);
+        alien1.setName("Phoenix");
+        alien1.setColour("Blue");
         
-        Configuration con = new Configuration().configure().addAnnotatedClass(_1_Alien.class);
+        Configuration con = new Configuration().configure().addAnnotatedClass(Alien_1.class);
         ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
         SessionFactory sf = con.buildSessionFactory(reg);
         Session session = sf.openSession();
